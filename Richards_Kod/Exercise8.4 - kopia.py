76c9a4454358a27b4074b67d1d7b23a634174912
RichardBlucher <blucher.richard@gmail.com>
2022-02-10 11:58:53 +0100

    Bättre 8.4

--- Richards_Kod/Exercise8.4 - kopia.py
+++ Richards_Kod/Exercise8.4 - kopia.py
@@ -16,7 +16,7 @@ tk.attributes('-topmost', 0)
 canvas.place(x=res / 20, y=res / 20, height=res, width=res)
 ccolor = ['#17888E', '#C1D02B', '#9E00C9', '#D80000', '#E87B00', '#9F68D3', '#4B934F']
 
-N = 500
+N = 50
 l = 100
 R = 4
 Rf = 10
@@ -43,12 +43,18 @@ for i in range(N // 2):
 particles = []
 text = []
 
-sharkpos = np.zeros((2))
-sharkx=[50]
-sharky=[50]
-sharkv=1.8
-sharkphi=np.random.rand(len(sharkx))*2*np.pi
+N_shark=1
+r_shark=np.column_stack((0.0, 0.0))
+
+v_shark=1.8
+phi_shark= np.random.rand(N_shark) * 2 * np.pi
 shark = []
+
+def update_position(r, v, phi, dt):
+    r[:, 0] = (r[:, 0] + v * np.cos(phi) * dt + l) % (2 * l) - l
+    print((v * np.cos(phi) * dt + l) % (2 * l) - l)
+    r[:, 1] = (r[:, 1] + v * np.sin(phi) * dt + l) % (2 * l) - l
+    return r
 
 def distance(r1, r2, l):
     # print("X1: %5.3f, Y1: %5.3f, X2: %5.3f, Y2: %5.3f, deltaX: %5.3f, deltaY: %5.3f" % (r1[0], r1[1], r2[0], r2[1],
@@ -75,11 +81,11 @@ def cluster_coeff(r, Rf, N):
 
     return coeff / N
 
-for i in range(len(sharkx)):
-    shark.append(canvas.create_oval((sharkx[i] - R + l) * res / l / 2,
-                                        (sharky[i] - R + l) * res / l / 2,
-                                        (sharkx[i] + R + l) * res / l / 2,
-                                        (sharky[i] + R + l) * res / l / 2,
+for j in range(N_shark):
+    shark.append(canvas.create_oval((r_shark[j, 0] - R + l) * res / l / 2,
+                                        (r_shark[j, 1] - R + l) * res / l / 2,
+                                        (r_shark[j, 0] + R + l) * res / l / 2,
+                                        (r_shark[j, 1] + R + l) * res / l / 2,
                                         outline=ccolor[1], fill=ccolor[1]))
 for j in range(N):
     particles.append(canvas.create_oval((r[j, 0] - R + l) * res / l / 2,
@@ -87,13 +93,14 @@ for j in range(N):
                                         (r[j, 0] + R + l) * res / l / 2,
                                         (r[j, 1] + R + l) * res / l / 2,
                                         outline=ccolor[0], fill=ccolor[0]))
+    '''
     closeParticles = 0
     for k in range(N):
         if k != j:
             if distance(r[j, :], r[k, :], l) < Rf:
                 closeParticles = closeParticles + 1
 
-    text.append(canvas.create_text(r[j, 0] + R, r[j, 1], text=closeParticles))
+    text.append(canvas.create_text(r[j, 0] + R, r[j, 1], text=closeParticles)) '''
 
 GA = canvas.create_text(100, 20, text=1 / N * np.linalg.norm([np.sum(np.cos(phi)), np.sum(np.sin(phi))]))
 GC = canvas.create_text(100, 40, text=cluster_coeff(r, Rf, N))
@@ -109,33 +116,29 @@ clustCoeff = np.zeros(4000)
 f = 0
 
 for t in range(4000):
-
-    r[:, 0] = (r[:, 0] + v * np.cos(phi) * dt + l) % (2 * l) - l
-    r[:, 1] = (r[:, 1] + v * np.sin(phi) * dt + l) % (2 * l) - l
-    sharkx = (sharkx + sharkv * np.cos(sharkphi)*dt + l) % (2 * l) - l
-    sharky = (sharky + sharkv * np.sin(sharkphi)*dt + l) % (2 * l) - l
+    r=update_position(r,v,phi,dt)
+    r_shark=update_position(r_shark, v_shark, phi_shark, dt)
     sharkdists = np.minimum(np.sqrt(
-        ((r[:, 0]) % (2 * l) - sharkx % (2 * l)) ** 2 + ((r[:, 1]) % (2 * l) - sharky % (2 * l)) ** 2),
-        np.sqrt((r[:, 0] - sharkx) ** 2 + (r[:, 1] - sharky) ** 2))
-    #print(sharkdists)
-    #print(sharkdists.shape)
+        ((r[:, 0]) % (2 * l) - r_shark[0,0] % (2 * l)) ** 2 + ((r[:, 1]) % (2 * l) - r_shark[0,1] % (2 * l)) ** 2),
+        np.sqrt((r[:, 0] - r_shark[0,0]) ** 2 + (r[:, 1] - r_shark[0,1]) ** 2))
 
-    closest_fish = np.where(sharkdists == np.amin(sharkdists))[0]
 
-    if np.sqrt(((r[closest_fish, 0]) % (2 * l) - sharkx % (2 * l)) ** 2 + ((r[closest_fish, 1]) % (2 * l) - sharky % (2 * l)) ** 2)<np.sqrt((r[closest_fish, 0] - sharkx) ** 2 + (r[closest_fish, 1] - sharky) ** 2):
-        sharkphi = np.arctan2((r[closest_fish, 1]) % (2 * l) - sharky % (2 * l), (r[closest_fish, 0]) % (2 * l) - sharkx % (2 * l))
+    closest_fish = np.where(sharkdists == np.amin(sharkdists))[0] # Index of closest fish
+
+    if np.sqrt(((r[closest_fish, 0]) % (2 * l) - r_shark[0,0] % (2 * l)) ** 2 + ((r[closest_fish, 1]) % (2 * l) - r_shark[0,1] % (2 * l)) ** 2)<np.sqrt((r[closest_fish, 0] - r_shark[0,0]) ** 2 + (r[closest_fish, 1] - r_shark[0,1]) ** 2):
+        phi_shark = np.arctan2((r[closest_fish, 1]) % (2 * l) - r_shark[0, 1] % (2 * l), (r[closest_fish, 0]) % (2 * l) - r_shark[0, 0] % (2 * l))
     else:
-        sharkphi = np.arctan2((r[closest_fish, 1] - sharky), (r[closest_fish, 0] - sharkx))
+        phi_shark = np.arctan2((r[closest_fish, 1] - r_shark[0, 1]), (r[closest_fish, 0] - r_shark[0, 0]))
 
 
 
     #sharkphi = np.arctan2(ydiff[yIndex] , xdiff[xIndex])
     for j in range(len(shark)):
         canvas.coords(shark[j],
-                    (sharkx[j] - R + l) * res / l / 2,
-                    (sharky[j] - R + l) * res / l / 2,
-                    (sharkx[j] + R + l) * res / l / 2,
-                    (sharky[j] + R + l) * res / l / 2,)
+                      (r_shark[j, 0] - R + l) * res / l / 2,
+                      (r_shark[j, 1] - R + l) * res / l / 2,
+                      (r_shark[j, 0] + R + l) * res / l / 2,
+                      (r_shark[j, 1] + R + l) * res / l / 2, )
     for j in range(N):
         canvas.coords(particles[j],
                       (r[j, 0] - R + l) * res / l / 2,
@@ -145,7 +148,7 @@ for t in range(4000):
         if j == closest_fish:
             canvas.itemconfig(particles[j], fill=ccolor[2])
         else:
-            canvas.itemconfig(particles[j], fill=ccolor[0])
+            canvas.itemconfig(particles[j], fill=ccolor[0]) # Change color of fish closest to shark
         closeParticles = 0
         interact = np.array([j])
         sumTime = np.zeros(N)
@@ -155,13 +158,13 @@ for t in range(4000):
                           np.sqrt((r[:, 0] - r[j, 0]) ** 2 + (r[:, 1] - r[j, 1]) ** 2))
         interact = dist < Rf
         if sharkdists[j] < Rf:
-            if np.sqrt(((r[j, 0]) % (2 * l) - sharkx % (2 * l)) ** 2 + (
-                    (r[j, 1]) % (2 * l) - sharky % (2 * l)) ** 2) < np.sqrt(
-                    (r[j, 0] - sharkx) ** 2 + (r[j, 1] - sharky) ** 2):
-                phi[j] = np.arctan2((r[j, 1]) % (2 * l) - sharky % (2 * l),
-                                      (r[j, 0]) % (2 * l) - sharkx % (2 * l))
+            if np.sqrt(((r[j, 0]) % (2 * l) - r_shark[0,0] % (2 * l)) ** 2 + (
+                    (r[j, 1]) % (2 * l) - r_shark[0,1] % (2 * l)) ** 2) < np.sqrt(
+                    (r[j, 0] - r_shark[0,0]) ** 2 + (r[j, 1] - r_shark[0,1]) ** 2):
+                phi[j] = np.arctan2((r[j, 1]) % (2 * l) - r_shark[0,1] % (2 * l),
+                                      (r[j, 0]) % (2 * l) - r_shark[0,0] % (2 * l))
             else:
-                phi[j] = np.arctan2((r[j, 1] - sharky), (r[j, 0] - sharkx))
+                phi[j] = np.arctan2((r[j, 1] - r_shark[0,1]), (r[j, 0] - r_shark[0,0]))
         else:
             phi[j] = np.angle(np.sum(np.exp(phi[interact] * 1j))) + eta * np.random.uniform(-1 / 2, 1 / 2)
 
@@ -185,8 +188,8 @@ for t in range(4000):
         '''
 
         # print(closeParticles)
-        canvas.coords(text[j], (r[j, 0] + l) * res / l / 2, (r[j, 1] + l) * res / l / 2)
-        canvas.itemconfig(text[j], text=closeParticles)
+        #canvas.coords(text[j], (r[j, 0] + l) * res / l / 2, (r[j, 1] + l) * res / l / 2)
+        #canvas.itemconfig(text[j], text=closeParticles)
 
 
 
