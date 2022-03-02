e5e4d4e20c8e2d7415d1b081ab9d0793ba45212a
Andreas <baunera@student.chalmers.se>
2022-03-02 10:22:33 +0100

    Some small fixes in statistics.py.

--- main/statistics.py
+++ main/statistics.py
@@ -1,24 +1,20 @@
 # Program som  beräknar medelvärden och std
 import numpy as np
 import matplotlib.pyplot as plt
-import several_predators
+import several_predators as sp
 
 n = 5  # Antal observationer
 
-several_predators.main()
-x = np.load('fish_eaten.npy')
-simulation_iterations = x.shape[0]
-data = np.zeros((simulation_iterations, n))
-data[:, 0] = x[:, 0]
-t = x[:, 1]
+data = np.zeros((sp.simulation_iterations, n))
+t = np.linspace(0, sp.simulation_iterations*sp.time_step, sp.simulation_iterations)
 
 def process(i):
-    several_predators.main()
+    sp.main()
     x = np.load('fish_eaten.npy')
     global data
     data[:, i] = x[:, 0]
 
-for i in range(1, n):
+for i in range(n):
     process(i)
 fish_eaten_mean = np.mean(data, axis=1)
 fish_eaten_std = np.std(data, axis=1)
