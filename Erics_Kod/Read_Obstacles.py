46c785c987e6c4f803e3075bb84bfdd25ebd6ba9
deric2019 <deric@student.chalmers.se>
2022-03-02 08:47:23 +0100

    Commit AvoidanceTest

--- Erics_Kod/Read_Obstacles.py
+++ Erics_Kod/Read_Obstacles.py
@@ -13,11 +13,12 @@ with open('Obstacles', 'r') as filestream:
     for line in filestream: # Read every row
         if line is not "\n":
             currentline = line.split(',')
-            circ_obst_coords.append( [float(currentline[0]), float(currentline[1])] )
-            circ_obst_radius.append(float(currentline[2]))
-
-            rect_obst_coords.append( [float(currentline[3]), float(currentline[4])] )
-            rect_obst_width_height.append( [float(currentline[5]), float(currentline[6])] )
+            if('None' not in currentline[:3]):
+                circ_obst_coords.append( [float(currentline[0]), float(currentline[1])] )
+                circ_obst_radius.append(float(currentline[2]))
+            if('None'not in currentline[3:]):
+                rect_obst_coords.append( [float(currentline[3]), float(currentline[4])] )
+                rect_obst_width_height.append( [float(currentline[5]), float(currentline[6])] )
 
 
 print(circ_obst_coords)
