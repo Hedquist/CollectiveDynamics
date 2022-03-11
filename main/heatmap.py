f3a0639e6424b1e63e681662e8cd4816733daf6b
Andreas <baunera@student.chalmers.se>
2022-03-11 10:44:45 +0100

    Snygga till lite

--- main/heatmap.py
+++ main/heatmap.py
@@ -6,20 +6,19 @@ import simple_torque as st
 
 fish_turning_speed = np.linspace(0.01, 1, 100)  # Fiskarnas turning speed
 shark_turning_speed = np.linspace(0.01, 1, 100)  # Hajarnas turning speed
+N = 5
 
 n = len(fish_turning_speed)
 fish_eaten_matrix = np.zeros((n, n))  # Allokera minne
 
 i = 0
 for fts in fish_turning_speed:
-    fts = np.trunc(100 * fts) * 0.01  # Ta bort trunkeringsfel
     j = 0
     for sts in shark_turning_speed:
-        sts = np.trunc(100 * sts) * 0.01  # Ta bort trunkeringsfel
-        res = 0
-        for k in range(5):
+        res = 0.0
+        for k in range(N):
             res += st.main(fts, sts)  # Anropa simulationen med olika turning speed
-        res /= 10
+        res /= N
         fish_eaten_matrix[i, j] = res  # Medelvärde av antal ätna fiskar
         j = j + 1
     i = i + 1
