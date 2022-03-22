78257905c4d436e832960e4042c35830994168ac
Andreas <baunera@student.chalmers.se>
2022-03-22 10:16:21 +0100

    Check för progress etc

--- main/heatmap.py
+++ main/heatmap.py
@@ -1,32 +1,34 @@
 # Skapa en heatmap för antal fiskar ätna för olika turning speeds
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import time
 import simple_torque as st
 
-fish_turning_speed = np.linspace(0.01, 1, 10)  # Fiskarnas turning speed
-shark_turning_speed = np.linspace(0.01, 1, 10)  # Hajarnas turning speed
-N = 5
+start = time.time()
+num_of_points= 5
+N = 3
 
-n = len(fish_turning_speed)
-fish_eaten_matrix = np.zeros((n, n))  # Allokera minne
+fish_turning_speed = np.linspace(0.4, 0.6, num_of_points)  # Fiskarnas turning speed
+shark_turning_speed = np.linspace(0.4, 0.6, num_of_points)  # Hajarnas turning speed
+fish_eaten_matrix = np.zeros((num_of_points, num_of_points))  # Allokera minne
 
 i = 0
 for fts in fish_turning_speed:
     j = 0
+    print('Progress = ', i/num_of_points*100, '%')
     for sts in shark_turning_speed:
         res = 0.0
         for k in range(N):
             temp = st.main(fts, sts)
             res += temp  # Anropa simulationen med olika turning speed
         res /= N
-        fish_eaten_matrix[i, j] = res  # Medelvärde av antal ätna fiskar
+        fish_eaten_matrix[j, i] = res  # Medelvärde av antal ätna fiskar
         j = j + 1
     i = i + 1
 
-# rng = np.random.default_rng()
-# fish_eaten_matrix = rng.integers(low=0, high=st.fish_count-4, size=n*n).reshape((n, n))
-plt.imshow(fish_eaten_matrix, interpolation='nearest', origin='lower')
+duration = time.time() - start
+print('Time [s]: ', duration)
+plt.imshow(fish_eaten_matrix, interpolation='spline16', origin='lower')
 plt.colorbar()
 plt.show()
-np.save('fish_eaten_matrix.npy', fish_eaten_matrix)
