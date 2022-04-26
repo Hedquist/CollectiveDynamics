91b043274dd79d3f400bb9770e92bda997712b5d
Hedquist <vilhelm.hedquist@hotmail.com>
2022-04-26 13:54:33 +0200

    fixed crash on non square matrixes

--- main/data_capture.py
+++ main/data_capture.py
@@ -7,14 +7,14 @@ num_times_run = 1
 seed = [n for n in range(num_times_run)]
 
 
-start_fish_turn = 0
-end_fish_turn = 0.1
-start_shark_turn = 0
-end_shark_turn = 0.1
+start_fish_turn = 0.01
+end_fish_turn = 0.08
+start_shark_turn = 0.01
+end_shark_turn = 0.04
 
 start = time.time()
 fish_turns = np.linspace(start_fish_turn, end_fish_turn, 4)  # Ger start till end
-shark_turns = np.linspace(start_shark_turn, end_shark_turn, 4)  # Ger start till end
+shark_turns = np.linspace(start_shark_turn, end_shark_turn, 2)  # Ger start till end
 num_of_points = len(fish_turns)
 print(fish_turns, 'fish turn speed')
 print(shark_turns, 'shark turn speed')
@@ -34,9 +34,10 @@ if new_simulation:
             for k in range(num_times_run):
                 temp = st.main(fts, sts, False, seed[k])
                 print('FTS:', fts, ' STS:', sts, ' Fiskar ätna:', temp)
+                print(fish_eaten_matrix)
                 res += temp  # Anropa simulationen med olika turning speed
             res /= num_times_run
-            fish_eaten_matrix[j, i] = res  # Medelvärde av antal ätna fiskar
+            fish_eaten_matrix[i, j] = res  # Medelvärde av antal ätna fiskar
             j = j + 1
             np.save('fish_eaten_matrix.npy', fish_eaten_matrix)
             # Print how far through the simulation we are currently
@@ -54,6 +55,7 @@ if new_simulation:
 else:
     fish_eaten_matrix = np.load('fish_eaten_matrix.npy')
 
+fish_eaten_matrix = np.rot90(fish_eaten_matrix, 1)
 print(fish_eaten_matrix, 'final fish eaten matrix')
 heatmap = plt.imshow(fish_eaten_matrix, interpolation='spline16', origin='lower')
 plt.xlabel('Fish turn speed')
@@ -69,8 +71,8 @@ fig = plt.figure()
 ax1 = plt.pcolormesh(x_, y_, fish_eaten_matrix)
 plt.xlabel('Fish turn speed')
 plt.ylabel('Shark turn speed')
-plt.xticks(np.arange(start_obst_count, end_obst_count+1, step=1))  # Set label locations.
-plt.yticks(np.arange(start_obst_radius, end_obst_radius+1, step=3))  # Set label locations.
+plt.xticks(np.arange(start_fish_turn, end_fish_turn+1, step=1))  # Set label locations.
+plt.yticks(np.arange(start_shark_turn, end_shark_turn+1, step=3))  # Set label locations.
 cbar = plt.colorbar(ax1)
 cbar.set_label('Average fish eaten', rotation=270, labelpad=15)
 plt.show()
